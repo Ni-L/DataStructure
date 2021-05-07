b50500e5aec98aa2404c7194a1aac0646ad29f8d
unknown <nilimawadal1@gmail.com>
2021-05-07 23:52:40 +0530

    [Nilima] Adding At Last

--- DataStructure-Linked-List/DataStructure-Linked-List/LinkedList.cs
+++ DataStructure-Linked-List/DataStructure-Linked-List/LinkedList.cs
@@ -5,8 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStructure_Linked_List
-{
+{/// <summary>
+/// Creating Linked List for Adding Elements 
+/// </summary>
     class LinkedList
     {
+        //Creating a node Equals to head, which will be pointing to the first element in linkedlist
+        public Node head;
+        public void InsertData(int new_data)
+        {
+            //Creating a object of node and adding data in node
+            Node new_node = new Node(new_data);
+            //if Head Equals to Null Then Add data 
+            //fills the first position in linkedlist, when head points to nothing
+
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = new_node;
+            }
+            Console.WriteLine("inserted into list" + new_node.data);
+        }
+            public Node GetLastNode()
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                return temp;
+            }
+
+            internal void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Linked list is empty");
+                    return;
+                }
+                else
+                {
+                    while (temp.next != null)
+                    {
+                        Console.Write(temp.data + "->");
+                        temp = temp.next;
+                    }
+                    Console.WriteLine(temp.data);
+                }
+            }
     }
 }
--- DataStructure-Linked-List/DataStructure-Linked-List/Program.cs
+++ DataStructure-Linked-List/DataStructure-Linked-List/Program.cs
@@ -5,11 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStructure_Linked_List
-{
+{/// <summary>
+/// Adding Method
+/// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("**********WELCOME TO LINKED LIST********");
+            LinkedList list = new LinkedList();
+            list.InsertData(56);
+            list.InsertData(30);
+            list.InsertData(70);
+            list.Display();
+            Console.ReadLine();
         }
     }
 }
