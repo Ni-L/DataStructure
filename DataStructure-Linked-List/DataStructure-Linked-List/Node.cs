6f67e13cdeea8417dd3fdce4e32a6236ca4ce0b9
unknown <nilimawadal1@gmail.com>
2021-05-07 23:37:52 +0530

    [Nilima] Added Linked List

---
+++ DataStructure-Linked-List/DataStructure-Linked-List/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Linked_List
+{/// <summary>
+/// Creating Node Class
+/// </summary>
+    class Node
+    {
+        //Data to be enter
+        public int data;
+        //finding next node
+        public Node next;
+
+        //Adding Node Method
+        //constructor call node class
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
